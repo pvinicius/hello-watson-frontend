310c144fd996b384edb7b63923fea879b5106291
Pedro Correia <peviniciusac@gmail.com>
2019-07-14 18:39:05 -0300

    finalizado show image

--- visual-recognition/src/components/show-image/show-image.component.js
+++ visual-recognition/src/components/show-image/show-image.component.js
@@ -25,13 +25,32 @@ export default class ShowImage extends React.Component {
             });
     }
 
+    onClickImage(imagePath) {
+        //encode - btoa
+        //decode - atob
+        //JSON.stringify
+
+        imagePath = btoa(imagePath);
+        imagePath = JSON.stringify(imagePath);
+
+        axios.post("https://localhost:44341/api/Recognition/classify", imagePath, { headers: { 'Content-Type': 'application/json' } })
+            .then((response) => {
+
+            })
+            .catch((error) => {
+
+            })
+    }
+
     render() {
         return (
             <div>
                 {
                     this.state.images.map(image => {
                         return (
-                            <img key={image.imageId} alt="" width="300" height="250" src={image.imagePath}></img>
+                            <div key={image.imageId} onClick={this.onClickImage.bind(this, image.imagePath)}>
+                                <img alt="" width="300" height="250" src={image.imagePath}></img>
+                            </div>
                         )
                     })
                 }
